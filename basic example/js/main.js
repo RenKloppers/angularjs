e25e609f9fd67a9ac45b1c774c0b2737a9c65527
Ren Kloppers <rkloppers@gmail.com>
2015-06-29 13:59:04 +0200

    adding defining a method on the scope example

--- basic example/js/main.js
+++ basic example/js/main.js
@@ -1,4 +1,17 @@
-angular.module("myapp", [])
-    .controller('FirstCtrl',function($scope) {
-        $scope.data= {message:"hello"};
-    });
+var myApp = angular.module('myapp', []);
+
+myApp.factory('Data', function(){
+    return {message:"data from service"}
+})
+
+myApp.controller('FirstCtrl', function($scope, Data){
+    $scope.data = Data;
+})
+
+myApp.controller('SecondCtrl', function($scope, Data){
+    $scope.data = Data;
+
+    $scope.reversedMessage = function(message){
+        return message.split("").reverse().join("");
+    }
+})
