61171f887c2bd2c312bd33b894f052e67ba8a36d
Ren Kloppers <rkloppers@gmail.com>
2015-06-29 14:24:58 +0200

    adding filter example

--- basic example/js/main.js
+++ basic example/js/main.js
@@ -4,6 +4,12 @@ myApp.factory('Data', function(){
     return {message:"data from service"}
 })
 
+myApp.filter('reverse', function(Data){
+    return function(text){
+        return text.split("").reverse().join("") + " " + Data.message;
+    }
+})
+
 myApp.controller('FirstCtrl', function($scope, Data){
     $scope.data = Data;
 })
@@ -14,4 +20,8 @@ myApp.controller('SecondCtrl', function($scope, Data){
     $scope.reversedMessage = function(message){
         return message.split("").reverse().join("");
     }
+})
+
+myApp.controller('ThirdCtrl', function($scope, Data){
+    $scope.data = Data;
 })
