953056ff744037c62999bcbb6d5f4b2657effcd5
Ren Kloppers <rkloppers@gmail.com>
2015-07-02 16:29:23 +0200

    adding working $routeProvider example

---
+++ basic_example/libs/mainNgView.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = angular.module("app", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl:"app.html",
+        controller:"AppCtrl"
+    })
+})
+
+app.controller('AppCtrl', function($scope){
+    $scope.model = {
+        message: "This is my app!!!"
+    }
+})
