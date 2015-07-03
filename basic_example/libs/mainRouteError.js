a2d0cc8d1b4426d08cdee8367f6efae5f52ba489
Ren Kloppers <rkloppers@gmail.com>
2015-07-03 11:36:02 +0200

    added directive to handle error display on screen example

--- basic_example/libs/mainRouteError.js
+++ basic_example/libs/mainRouteError.js
@@ -14,6 +14,19 @@ app.config(function($routeProvider){
         })
 })
 
+//note is not good practise to pass in $rootScope into a directive! example only
+app.directive("error", function($rootScope){
+    return{
+        restrict: "E",
+        template: '<div class="alert-box alert" ng-show="isError">Error!!!</div>',
+        link: function (scope){
+            $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
+                scope.isError = true;
+            })
+        }
+    }
+})
+
 app.controller('AppCtrl', function($rootScope){
     $rootScope.$on("$routeChangeError", function(event, current, previous, rejection){
         console.log(event);
